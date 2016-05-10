30fc0caae468384ff3abb03769b5f9472b4c8d61
anna <annjellyiu5@gmail.com>
2016-05-10 08:29:20 +0300

    new menu

--- app/src/main/java/duality/questmanager/fragments/CreateTaskFragment.java
+++ app/src/main/java/duality/questmanager/fragments/CreateTaskFragment.java
@@ -2,6 +2,7 @@ package duality.questmanager.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
@@ -16,17 +17,23 @@ import java.util.ArrayList;
 import duality.questmanager.R;
 import duality.questmanager.Task;
 import duality.questmanager.rest.CreateTask;
-
+import com.wdullaer.materialdatetimepicker.date.DatePickerDialog;
+import com.wdullaer.materialdatetimepicker.time.RadialPickerLayout;
+import com.wdullaer.materialdatetimepicker.time.TimePickerDialog;
+import java.util.Calendar;
 
 /**
  * Created by anna on 07.05.16.
  */
-public class CreateTaskFragment extends Fragment {
+public class CreateTaskFragment extends Fragment implements TimePickerDialog.OnTimeSetListener,
+        DatePickerDialog.OnDateSetListener{
 
     private EditText title;
     private TextView countTitleTextView;
     private EditText details;
     private TextView countDetailsTextView;
+    private TextView timeTextView;
+    private TextView dateTextView;
 
     public CreateTaskFragment(){}
 
@@ -45,6 +52,9 @@ public class CreateTaskFragment extends Fragment {
         details = (EditText) rootView.findViewById(R.id.createTaskDetails);
         countDetailsTextView = (TextView) rootView.findViewById(R.id.countDetailsTextView);
         countDetailsTextView.setText("120/120");
+
+        timeTextView = (TextView)rootView.findViewById(R.id.time_textview);
+        dateTextView = (TextView)rootView.findViewById(R.id.date_textview);
 
         title.addTextChangedListener(new TextWatcher() {
             @Override
@@ -76,6 +86,16 @@ public class CreateTaskFragment extends Fragment {
                 countDetailsTextView.setText(120 - s.toString().length() + "/120");
             }
         });
+
+
+        Calendar now = Calendar.getInstance();
+        DatePickerDialog dpd = DatePickerDialog.newInstance(
+                CreateTaskFragment.this,
+                now.get(Calendar.YEAR),
+                now.get(Calendar.MONTH),
+                now.get(Calendar.DAY_OF_MONTH)
+        );
+        dpd.show(getActivity().getFragmentManager(), "Datepickerdialog");
         return rootView;
 
     }
@@ -88,4 +108,15 @@ public class CreateTaskFragment extends Fragment {
 
     }
 
+    @Override
+    public void onDateSet(DatePickerDialog view, int year, int monthOfYear, int dayOfMonth) {
+        String date = "You picked the following date: "+ dayOfMonth+"/"+(monthOfYear+1)+"/"+year;
+        dateTextView.setText(date);
+    }
+
+    @Override
+    public void onTimeSet(RadialPickerLayout view, int hourOfDay, int minute, int second) {
+        String time = "You picked the following time: "+hourOfDay+"h"+minute;
+        timeTextView.setText(time);
+    }
 }
