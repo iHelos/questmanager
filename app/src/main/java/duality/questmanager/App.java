db633a4f70329fddb117ada3141cdcc95771adb2
iHelos <ihelos.ermakov@gmail.com>
2016-05-25 10:42:28 +0300

    logo bug fixed #2

--- app/src/main/java/duality/questmanager/App.java
+++ app/src/main/java/duality/questmanager/App.java
@@ -8,21 +8,21 @@ public class App extends Application {
     public void onCreate() {
         super.onCreate();
 
-        VectorDrawableCompat.enableResourceInterceptionFor(getResources(),
-                R.drawable.check_circle_outline,
-                R.drawable.checkbox_blank_circle_outline__1_,
-                R.drawable.close_circle_outline,
-                R.drawable.coin,
-                R.drawable.inbox,
-                R.drawable.information,
-                R.drawable.logout_variant,
-                R.drawable.menu,
-                R.drawable.outbox,
-                R.drawable.pencil,
-                R.drawable.plus,
-                R.drawable.settings
+//        VectorDrawableCompat.enableResourceInterceptionFor(getResources(),
+//                R.drawable.check_circle_outline,
+//                R.drawable.checkbox_blank_circle_outline__1_,
+//                R.drawable.close_circle_outline,
+//                R.drawable.coin,
+//                R.drawable.inbox,
+//                R.drawable.information,
+//                R.drawable.logout_variant,
+//                R.drawable.menu,
+//                R.drawable.outbox,
+//                R.drawable.pencil,
+//                R.drawable.plus,
+//                R.drawable.settings
 //                R.drawable.login_logo
-                );
+//                );
 
         // вызов VectorDrawableCompat.enableResourceInterceptionFor()
     }
